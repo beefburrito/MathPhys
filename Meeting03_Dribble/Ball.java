d06ecce75b7fce9ecde9b583a5b7d2ea6064bcd2
beefburrito <eugenesebastian1234@gmail.com>
2019-06-26 14:50:39 +0700

    Completed To-Do 6

--- Meeting03_Dribble/Ball.java
+++ Meeting03_Dribble/Ball.java
@@ -1,4 +1,4 @@
-package Meeting03_Dribble;
+//package Meeting03_Dribble;
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -11,24 +11,39 @@ public class Ball {
     private double velocityX;                   // ball's velocity
     private double velocityY;
     private Color ballColor;
-    private final static double e = 0.9;        // ball's coefficient of resistution
-    private final static double GRAVITY = 0.5;  // use custom gravity
+    private double e = 0.5;        // ball's coefficient of resistution
+    private double GRAVITY = 0.5;  // use custom gravity
 
-    public Ball(double positionX, double positionY, double radius, double velocityX, double velocityY, Color ballColor) {
+    public Ball(double positionX, double positionY, double radius, double velocityX, double velocityY, Color ballColor, double elasticity) {
         this.radius = radius;
         this.positionX = positionX;
         this.positionY = positionY;
         this.velocityX = velocityX;
         this.velocityY = velocityY;
         this.ballColor = ballColor;
+        this.e = elasticity;
     }
-
+    // Getters
+    public double getX() {
+        return positionX;
+    }
+    public double getY() {
+        return positionY;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public double getE() {
+        return e;
+    }
+    public void toggleGravity() {
+        if(GRAVITY != 0) GRAVITY = 0;
+        else GRAVITY = 0.5;
+    }
     // drawing function
     public void draw(Graphics g) {
-        Color tempColor = g.getColor();
         g.setColor(ballColor);
         g.fillOval((int) (positionX - radius), (int) (positionY - radius), (int) (2 * radius), (int) (2 * radius));
-        g.setColor(tempColor);
     }
 
     // move the ball by modifying current position, with assumption that time = 1
@@ -45,7 +60,7 @@ public class Ball {
                 double error = radius - w.distanceFromPoint(positionX, positionY);
 
                 positionX += error * w.normalLine().getX();
-                positionY += error * w.normalLine().getY();
+                positionY -= error * w.normalLine().getY();
 
                 //if the ball collided with a vertical wall
                 if(w.getWidth() == 0) {
@@ -55,6 +70,14 @@ public class Ball {
                 //if the ball collided with a horizontal wall
                 else if(w.getHeight() == 0) {
                     velocityY = -e * velocityY;
+                }
+                // if the wall is slanted/diagonal
+                else {
+                    Vector normal = w.normalLine(); // Get the wall's normal vector
+                    double c = velocityX*normal.getX() + velocityY*normal.getY();
+                    Vector proj = new Vector(c*normal.getX(), c*normal.getY()); 
+                    velocityX = 2*proj.getX() - velocityX; // V' = 2*proj - v
+                    velocityY = 2*proj.getY() - velocityY;
                 }
             }
         }
